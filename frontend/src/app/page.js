679e99b48a1b845b699bdc8e10733ef9ca523dd9
KuukuaSwanzy <nhyiraswanzy11@gmail.com>
2025-09-29 19:03:27 +0000

    uploaded frontpage_picture.webp

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,10 +2,8 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import NextLink from 'next/link';
-import ProTip from '@/components/ProTip';
-import Copyright from '@/components/Copyright';
+
+
 
 export default function Home() {
   return (
@@ -20,13 +18,8 @@ export default function Home() {
         }}
       >
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in JavaScript
+          Spark and Blush 2.0
         </Typography>
-        <Link href="/about" color="secondary" component={NextLink}>
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
