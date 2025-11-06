5bfec656aef405ad8ffbd50ac500c108c235beb8
Klenam <ayokwashie42@gmail.com>
2025-11-06 04:21:32 +0000

    bug fixes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <Homepage />
       <AboutPage />
       <SpecialOffers />
-      <Services />
+      {/* <Services /> */}
       <Products />
       <Contact />
     </>
