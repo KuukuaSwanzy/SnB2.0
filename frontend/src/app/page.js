83c3a2c4d5407dd071123d1ed17787ae3bf79b23
Klenam <ayokwashie42@gmail.com>
2025-11-06 04:16:37 +0000

    bug fixes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 // import Typography from "@mui/material/Typography";
 import AboutPage from "./(web)/about";
-import Homepage from "./(web)/homepage";
+import Homepage from "./(web)/Homepage";
 import SpecialOffers from "./(web)/SpecialOffers";
-import Services from "./(web)/services";
-import Products from "./(web)/products";
+import Services from "./(web)/Services";
+import Products from "./(web)/Products";
 import Contact from "./(web)/contact";
 
 export default function Home() {
